8b8f4893f9c3ae71ea0eeb894343e8f29ff6960b
MohamedAlaga <malaga.142014@gmail.com>
2024-01-21 11:57:39 +0200

    task 0

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ FizzBuzz
-    Change of logic if (i % 3) == 0 and (i % 5) == 0:
 """
 import sys
 
@@ -18,10 +17,12 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
+        if i == 15 :
             tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
+        elif (i % 3) == 0 :
             tmp_result.append("Fizz")
+        elif (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
@@ -38,3 +39,4 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
+    
